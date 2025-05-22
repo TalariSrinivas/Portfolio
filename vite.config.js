77f308063313cdf629a6e239866578c4b406c419
TalariSrinivas <165267278+TalariSrinivas@users.noreply.github.com>
2025-05-22 13:52:41 +0530

    Your commit message here

--- vite.config.js
+++ vite.config.js
@@ -2,26 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
+  plugins: [react(),
     tailwindcss(),
   ],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react')) {
-              return 'vendor_react'
-            }
-            if (id.includes('lodash')) {
-              return 'vendor_lodash'
-            }
-            return 'vendor'
-          }
-        }
-      }
-    }
-  }
 })
